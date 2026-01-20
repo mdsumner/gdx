f0a4ff4a6245db21c6151831aa3ff8b6cfaf4cb8
Michael Sumner <mdsumner@gmail.com>
2026-01-20 05:06:47 +0000

    fix some indexing and added CFTime for "time"

--- gdx/gdx.py
+++ gdx/gdx.py
@@ -1,6 +1,8 @@
 import xarray as xr
 from xarray.backends import BackendEntrypoint, BackendArray
 from xarray.core import indexing
+from xarray.coding.times import decode_cf_datetime
+
 import numpy as np
 import dask.array as da
 from osgeo import gdal
@@ -168,6 +170,7 @@ class GDALMultiDimArray(BackendArray):
         # Get numpy dtype
         gdal_dtype = mdarray.GetDataType().GetNumericDataType()
         self._dtype = GDALBackendArray._gdal_to_numpy_dtype(gdal_dtype)
+        self._chunks = tuple(mdarray.GetBlockSize()) 
     
     @property
     def shape(self):
@@ -247,7 +250,9 @@ class GDALMultiDimArray(BackendArray):
             count=counts,
             options=[f"CACHE_SIZE={str(num_bytes)}"]
       )
-      
+      print(f"starts={starts}, counts={counts}, steps={steps}, shape={self.shape}, chunks={self._chunks}")
+
+        
       data = self.mdarray.ReadAsArray(
           array_start_idx=starts,
           count=counts,
@@ -461,6 +466,12 @@ class GDALBackendEntrypoint(BackendEntrypoint):
             if is_coord and len(dim_names) == 1:
                 # Add as coordinate - load eagerly for index variables
                 coord_data = backend_array[:]  # Load the data
+                    # Decode CF time coordinates
+                if array_name == 'time':
+                  units = mdarray.GetUnit()
+                  calendar = attrs.get('calendar', 'standard')
+                  if units:
+                     coord_data = decode_cf_datetime(coord_data, units, calendar)
                 coords[array_name] = xr.DataArray(coord_data, dims=dim_names, attrs=attrs)
             else:
                 # Add as data variable
